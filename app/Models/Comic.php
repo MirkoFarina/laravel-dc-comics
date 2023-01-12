7b40b794dfda31a09aaf844a28c501e000e091a3
MirkoFarina <MirkoFarina@outlook.com>
2023-01-12 18:22:55 +0100

    [add] seeder e popolato db, tramite file in config

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+// USE STR FOR GENERATE THE SLUG
+use Illuminate\Support\Str;
 
 class Comic extends Model
 {
+    public static function generator_slug ($title){
+        $slug = Str::slug($title,'-');
+        $original_slug = $slug;
+        $c = 1;
+
+        $slug_exists = Comic::where('slug', $slug)->first();
+
+        while($slug_exists) {
+            $slug = $original_slug . '-' . $c;
+            $slug_exists = Comic::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
     use HasFactory;
 }
--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +15,19 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        $data_comics = config('db.comics') ;
-        dd($data_comics);
+        $data_comics = config('db');
+
+        foreach ($data_comics as $comic) {
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->slug = Comic::generator_slug($new_comic->title);
+            $new_comic->description = $comic['description'];
+            $new_comic->image = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+            $new_comic->save();
+        }
     }
 }
