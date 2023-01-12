dc8865275089210ac165452771f411b11b567f97
MirkoFarina <MirkoFarina@outlook.com>
2023-01-12 18:04:24 +0100

    [add] migration to db

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data_comics = config('db.comics') ;
+        dd($data_comics);
+    }
+}
